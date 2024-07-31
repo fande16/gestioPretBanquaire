8ca05e06961c9fbbe9a29f784433a869961658b5
fande16 <fande.ndiaye@gmail.com>
2024-07-31 23:33:45 +0000

    Add all CRUD

--- src/main/java/sn/edu/ugb/ipsl/gestionPretBanquaire/models/UserRole.java
+++ src/main/java/sn/edu/ugb/ipsl/gestionPretBanquaire/models/UserRole.java
@@ -1,20 +1,22 @@
 package sn.edu.ugb.ipsl.gestionPretBanquaire.models;
 
+
 import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
+
 public class UserRole {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     @ManyToOne
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
-    @Id
     @ManyToOne
     @JoinColumn(name = "role_id", nullable = false)
     private Role role;
