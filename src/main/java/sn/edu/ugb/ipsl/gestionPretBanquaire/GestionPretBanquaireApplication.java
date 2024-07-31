b2f64043a1405d3e53b7053e457e12230110b5cc
fande16 <fande.ndiaye@gmail.com>
2024-07-31 18:13:56 +0000

    first commit

---
+++ src/main/java/sn/edu/ugb/ipsl/gestionPretBanquaire/GestionPretBanquaireApplication.java
@@ -0,0 +1,13 @@
+package sn.edu.ugb.ipsl.gestionPretBanquaire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionPretBanquaireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionPretBanquaireApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sn/edu/ugb/ipsl/gestionPretBanquaire/GestionPretBanquaireApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.edu.ugb.ipsl.gestionPretBanquaire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionPretBanquaireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
